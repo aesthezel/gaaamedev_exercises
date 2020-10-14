bb17d92fa68c442117576589ca84f3740664e16e
David Pino <aesthezel@gmail.com>
2020-10-14 10:08:14 -0400

    Probando MVC y ejercicio nueve

---
+++ csharp_course/examples/nine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace nine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 6)
+                {   
+                    Console.WriteLine("¡Estoy en la sexta iteración!");
+                    break;
+                }
+            }
+            Console.WriteLine("Se acabó el programa...");
+            Console.ReadKey();
+        }
+    }
+}
